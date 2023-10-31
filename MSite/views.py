5ee97d81becd97f59e1529aec183253ecae393c1
Randybutternugs <adonisalexis@icloud.com>
2023-10-30 23:33:01 -0400

    Super formatting views.py

--- MSite/views.py
+++ MSite/views.py
@@ -3,37 +3,63 @@ import random
 import logging
 import requests
 
+#Declare Blueprints for main app (views) and the error handler I think works(?) (error_handlers)
 views = Blueprint('views', __name__)
 error_handlers = Blueprint('error_handlers', __name__)
 
+#Function for selecting one of the gifs in static images to serve as a background for any page (used mainly for error pages)
 def get_gif_urls():
     return [url_for('static', filename=f'images/gif{i}.gif') for i in range(1, 4)]
+
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-  |
+#  GENERAL VIEWS                                                 |
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \ /
+
+
+
+#region =-=-=-=- Home Page -=-=-=-=
 
 @views.route('/')
 def home():
     return render_template("homepage.html")
 
-#@views.route('/bday')
-#def home():
-#    gif_path = random.choice(get_gif_urls())
-#    return render_template("makday.html", gif_path=gif_path)
+#endregion
 
+# -----------------
+
+
+#region =-=-=-=- About Page -=-=-=-=
 
 @views.route('/about')
 def about():
     return "This is the about page"
+
+#endregion
+
 
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-  |
 #  VIEW FOR TRIGGERING ERRORS FOLLOWED BY ERROR HANDLING ROUTES  |
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \ /
 
-# Make more as needed
-# USE ROUTE BELOW FOR TESTING ERROR CODES AND MAKING APPROPRIATE MESSAGES
 
+
+####region =-=-=-=- Error Testing Route -=-=-=-=
+
+
+# USE ROUTE BELOW FOR TESTING ERROR CODES AND MAKING APPROPRIATE MESSAGES
 # @views.route('/error') #Currently set to trigger a 500 error when I input this route
 # def trigger_forbidden():
 #     abort(500)
+
+####endregion 
+
+# -----------------
+
+####region =-=-=-=- 404 Error Handling Route -=-=-=-=
+
 
 # 404 error handler for this blueprint
 @error_handlers.app_errorhandler(404)
@@ -46,6 +72,12 @@ def not_found(e):
 
     return render_template("error.html", gif_path=gif_path, code_message_font=code_message_font), 404
 
+####endregion
+
+# -----------------
+
+####region =-=-=-=- 500 Error Handling Route -=-=-=-=
+
 # 500 error handler for this blueprint
 @error_handlers.app_errorhandler(500)
 def server_error(e):
@@ -55,3 +87,8 @@ def server_error(e):
     code_message_font = [500, "Unexpected Server-Side Exception ", "5vh"]
 
     return render_template("error.html", gif_path=gif_path, code_message_font=code_message_font), 500
+
+
+####endregion
+
+# -----------------
